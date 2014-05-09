fdf8e037eda989093c223b2254b3af0b5dfe6c55
rootslab <44gatti@gmail.com>
2014-05-09 10:22:14 +0200

    v 2.5.0. ops.. added #reveal test and example. On branch master new file:   example/reveal-example.js new file:   test/reveal-test.js

---
+++ example/reveal-example.js
@@ -0,0 +1,36 @@
+/* 
+ * Bolgia#reveal Example
+ */
+
+var log = console.log
+    , util = require( 'util' )
+    , Bolgia = require( '../' )
+    , reveal = Bolgia.reveal
+    , isArray = Array.isArray
+    // recursive conversion of items to Buffers
+    , convert = function ( arr ) {
+        var a = arr || []
+            , alen = a.length
+            , el = a[ 0 ]
+            , i = 0
+            ;
+        for ( ; i < alen; el = a[ ++i ] ) {
+            if ( isArray( el ) ) {
+                convert( el );
+                continue;
+            }
+            a[ i ] = new Buffer( String( el ) );
+        };
+        return a;
+    }
+    , nested_array = [
+        [ '1309448221456', '111', [ 'ELMICANEDU' ] ]
+        , [ '1309448128012', '1913', [ 'ELMIFRADELU', 'SCHEO', 'CIAO' ] ]
+        , [ [ '0xaa', '-100', '+100', 'aa' ] ]
+    ]
+    , nested_data = convert( nested_array )
+    , list = reveal( nested_data )
+    ;
+
+log( '\n- revealed array:\n' )
+log( util.inspect( list, false, 2, true ), '\n' );
---
+++ test/reveal-test.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+
+/* 
+ * Bolgia#reveal Test
+ */
+
+var log = console.log
+    , assert = require( 'assert' )
+    , util = require( 'util' )
+    , Bolgia = require( '../' )
+    , reveal = Bolgia.reveal
+    , isArray = Array.isArray
+    // recursive conversion of items to Buffers
+    , convert = function ( arr ) {
+        var a = arr || []
+            , alen = a.length
+            , el = a[ 0 ]
+            , i = 0
+            ;
+        for ( ; i < alen; el = a[ ++i ] ) {
+            if ( isArray( el ) ) {
+                convert( el );
+                continue;
+            }
+            a[ i ] = new Buffer( String( el ) );
+        };
+        return a;
+    }
+    , v1 = '1309448221456'
+    , v2 = '111'
+    , v3 = 'ELMICANEDU'
+    , v4 = '1913'
+    , v5 = 'ELMIFRADELU'
+    , v6 = 'SCHEO'
+    , v7 = 'CIAO'
+    , v8 = 'MIAO'
+    , v9 = '0xaa'
+    , v10 = '-100'
+    , v11 = '+100'
+    , v12 = 'aa'
+    , nested_array = [
+        [ v1, v2, [ v3 ] ]
+        , [ v4, v5, [ v6, v7, v8 ] ]
+        , [ [ v9, v10, v11, v12 ] ]
+    ]
+    , nested_data = convert( nested_array )
+    , expected_array = [
+        [ +v1, +v2, [ v3 ] ]
+        , [ +v4, v5, [ v6, v7, v8 ] ]
+        , [ [ +v9, +v10, +v11, v12 ] ]
+    ]
+    , list = reveal( nested_data )
+    ;
+
+log( '- created a nested Array of Buffers.' );
+log( '- array has been converted.' );
+log( '- expected array:', util.inspect( list, false, 2, true ) );
+
+log( '- deeply check results.' );
+assert.deepEqual( nested_array, expected_array );
+
+log( '- ok, results match. ' );
+log( '- revealed array:', util.inspect( list, false, 2, true ) );
