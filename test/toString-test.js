0fd150b7c42e0207c65372494a359e4d1af2f7b5
rootslab <44gatti@gmail.com>
2014-08-15 03:42:28 +0200

    v2.7.1. Fixed #flatten bug. Added #flatten test. fixed .* files.. On branch master modified:   .gitignore modified:   .npmignore modified:   .travis.yml modified:   lib/bolgia.js modified:   package.json new file:   test/flatten-simple-test.js modified:   test/run.sh modified:   test/toString-test.js

--- test/toString-test.js
+++ test/toString-test.js
@@ -11,20 +11,29 @@ var log = console.log
     , assert = require( 'assert' )
     , util = require( 'util' )
     , Bolgia = require( '../' )
+    , toString = Bolgia.toString
+    , ooo = Bolgia.circles
     , obj = {
         'NaN' : NaN
-        , 'empty string': ''
+        , 'empty_string': ''
+        , 'buffer' : new Buffer( 1 )
     }
     ;
 
 log( '- test toString with NaN, expected result is [object Null]' );
-assert.ok( Bolgia.toString( obj.NaN ) === Bolgia.circles.nul, 'wrong return type for NaN!' );
+assert.ok( toString( obj.NaN ) === ooo.nul, 'wrong return type for NaN!' );
 
 log( '- test toString with "", expected result is [object Null]' );
-assert.ok( Bolgia.toString( obj[ 'empty string' ] ) === Bolgia.circles.nul, 'wrong type for empty string!' );
+assert.ok( toString( obj.empty_string ) === ooo.nul, 'wrong type for empty string!' );
 
 log( '- enable custom types for toString, test with NaN, expected result is [object NaN]' );
-assert.ok( Bolgia.toString( obj.NaN, true ) === Bolgia.circles.nan, 'wrong return type for NaN!' );
+assert.ok( toString( obj.NaN, true ) === ooo.nan, 'wrong return type for NaN!' );
 
 log( '- test toString with "", expected result is [object NullString]' );
-assert.ok( Bolgia.toString( obj[ 'empty string' ], true ) === Bolgia.circles.nst, 'wrong type for empty string!' );
+assert.ok( toString( obj.empty_string, true ) === ooo.nst, 'wrong type for empty string!' );
+
+log( '- test toString with a Buffer Object, expected result is [object Buffer]' );
+assert.ok( toString( obj.buffer ) === ooo.buf, 'wrong return type for NaN!' );
+
+log( '- test toString with an Object, expected result is [object Object]' );
+assert.ok( toString( {} ) === ooo.obj, 'wrong return type for NaN!' );
