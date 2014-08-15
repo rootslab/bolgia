8d2d3c3b8b88b50171d3e7f646e2b5862dd07310
rootslab <44gatti@gmail.com>
2014-08-15 04:19:45 +0200

    v2.7.2. Fixed #count, count any array items as a single leaf. Added #count test for #flatten. Renamed some tests.. On branch master modified:   lib/bolgia.js modified:   package.json renamed:    test/flatten-simple-test.js -> test/flatten-count-test.js renamed:    test/some-edge-cases-test.js -> test/qs-some-edge-cases-test.js

---
+++ test/flatten-count-test.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+/* 
+ * Bolgia#flatten Simple Test
+ */
+
+var log = console.log
+    , assert = require( 'assert' )
+    , util = require( 'util' )
+    , inspect = util.inspect
+    , Bolgia = require( '../' )
+    , toString = Bolgia.toString
+    , flatten = Bolgia.flatten
+    , count = Bolgia.count
+    , ooo = Bolgia.circles
+    , keys = Object.keys
+    , obj = {
+        1 : {
+            'a' : null
+        }
+        , 2 : {
+            // empty objects are not added
+            'a' : {}
+            , 'b' : {
+                'feed' : 'beef'
+                , 'blah' : 4
+                , 'ciao' : null
+            }
+        }
+        , 3 : {
+            'a' : { 0 : undefined }
+        }
+        // empty arrays are not added
+        , 4 : []
+        , 5 : [ 1, 2, 3, 4, 5 ]
+        // pass Arguments type
+        , weird : ( function () { return arguments; } )( [ 'a', 'r', 'g', 's' ] )
+    }
+    , expected = {
+        '1[a]' : null
+        , '2[b][feed]' : 'beef'
+        , '2[b][blah]' : 4
+        , '2[b][ciao]' : null
+        , '3[a][0]' : undefined
+        , '5[0]' : 1
+        , '5[1]' : 2
+        , '5[2]' : 3
+        , '5[3]' : 4
+        , '5[4]' : 5
+        , 'weird[0][0]': 'a'
+        , 'weird[0][1]': 'r'
+        , 'weird[0][2]': 'g'
+        , 'weird[0][3]': 's'
+    }
+    ;
+
+log( '- check #flatten result for:\n %s,\n - expected result:\n %s.', inspect( obj, false, 10, true ), inspect( expected, false, 10, true ) );
+assert.deepEqual( flatten( obj ), expected, 'got: ' + inspect( flatten( obj ), false, 10, true ) );
+
+log( '- check if #count of source obj leafs is equal to %s.', inspect( keys( expected ).length ) );
+assert.equal( keys( expected ).length, count( expected ) );
+assert.equal( count( obj, true ), count( expected ) );
